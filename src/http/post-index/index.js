86575eeee5cfe159b1021d85035fb161f9eb0bf2
Saif Al Falah <saifalfalah@gmail.com>
2020-09-15 04:55:44 +0530

    structure of the app is better now. error handling is now happening

--- src/http/post-index/index.js
+++ src/http/post-index/index.js
@@ -6,16 +6,32 @@ exports.handler = async function http(req) {
   let body = parseBody(req);
   console.log(body);
 
-  let info = await ytdl.getInfo("wILAAYhKQnY");
-  info = info.formats;
-  let containsAudio;
-  containsAudio = info.filter((key) => key.container === "mp4");
-
-  return {
-    statusCode: 200,
-    headers: {
-      "content-type": "application/json; charset=utf8",
-    },
-    body: JSON.stringify(containsAudio),
-  };
+  try {
+    if (!body) throw new Error(600);
+    let info = await ytdl.getInfo("wILAAYhKQnY");
+    info = info.formats;
+    let containsAudio;
+    containsAudio = info.filter((key) => key.container === "mp4");
+    return {
+      statusCode: 200,
+      headers: {
+        "content-type": "application/json; charset=utf8",
+      },
+      body: JSON.stringify(containsAudio),
+    };
+  } catch (e) {
+    console.error(e.message);
+    let error = {
+      error: "Something blew up. Sorry.",
+    };
+    return {
+      headers: {
+        "content-type": "application/json; charset=utf8",
+      },
+      body: JSON.stringify(error),
+      statusCode: 400,
+    };
+  } finally {
+    // Save request data here
+  }
 };
