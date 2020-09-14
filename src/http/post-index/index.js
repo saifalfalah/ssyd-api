2a478c1a7efd0f24db418c55f1075d5a226cd776
Saif Al Falah <saifalfalah@gmail.com>
2020-09-15 04:34:09 +0530

    body parser is now working

--- src/http/post-index/index.js
+++ src/http/post-index/index.js
@@ -1,6 +1,11 @@
 const ytdl = require("ytdl-core");
+const arc = require("@architect/functions");
+const parseBody = arc.http.helpers.bodyParser;
 
 exports.handler = async function http(req) {
+  let body = parseBody(req);
+  console.log(body);
+
   let info = await ytdl.getInfo("wILAAYhKQnY");
   info = info.formats;
   let containsAudio;
